c85a7193f653ae28d70814102d91b2fa9ba6974a
Ali-Hassaan <56960392+Ali-Hassaan@users.noreply.github.com>
2020-06-02 18:39:41 +0500

    Add files via upload

---
+++ build/precache-manifest.d2542ea4363e0e3d5a171bd8df236e63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab37f9ac2689eadd6085d17a00e4263d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eecfb6568d12894d6f5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd6481eb5194d0fe544a",
+    "url": "/static/js/2.a95dc5dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a95dc5dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eecfb6568d12894d6f5",
+    "url": "/static/js/main.16005ecf.chunk.js"
+  },
+  {
+    "revision": "7650e95a5e6344b791be",
+    "url": "/static/js/runtime-main.44c28b23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
